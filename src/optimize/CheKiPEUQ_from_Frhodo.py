Fixed shape of weighting arrray passed to CheKiPEUQ
Now need to do cleanup of print statements in next commit.

src/optimize/CheKiPEUQ_from_Frhodo.py
@@ -64,8 +64,8 @@ def load_into_CheKiPUEQ(simulation_function, observed_data, pars_initial_guess =
     if len(observed_data_lower_bounds) > 0: #assume that both lower and upper bounds exist on data if there is a lower bounds array provided.
         UserInput.responses['responses_observed_uncertainties'] = extract_larger_delta_and_make_sigma_values(UserInput.responses['responses_observed'], observed_data_lower_bounds, observed_data_upper_bounds, sigma_multiple)   
     try:
-        weights_data = np.atleast_2d(weights_data).T
-        UserInput.responses['responses_observed_weighting'] = (weights_data*np.ones(num_responses)).T
+        #weights_data = np.atleast_2d(weights_data).T
+        UserInput.responses['responses_observed_weighting'] = weights_data #(weights_data*np.ones(num_responses)).T
     except:
         print("There was an error in the weightings in CheKiPEUQ_from_Frhodo processing.")
     UserInput.model['InputParameterPriorValues'] = pars_initial_guess

src/optimize/fit_fcn.py
@@ -422,11 +422,11 @@ class Fit_Fun:
             Bayesian_dict['simulation_function'] = get_last_obs_sim_interp #using wrapper that just returns the last_obs_sim_interp
             
             if np.size(loss_resid) == 1:  # optimizing single experiment
-                Bayesian_dict['weights_data'] = np.array(output_dict['aggregate_weights'], dtype=object)
+                Bayesian_dict['weights_data'] = np.array(output_dict['aggregate_weights'], dtype=object).flatten()
             else:
                 aggregate_weights = np.array(output_dict['aggregate_weights'], dtype=object)
                 exp_loss_weights = loss_exp/generalized_loss_fcn(loss_resid) # comparison is between selected loss fcn and SSE (L2 loss)
-                Bayesian_dict['weights_data'] = np.concatenate(aggregate_weights*exp_loss_weights, axis=0)
+                Bayesian_dict['weights_data'] = np.concatenate(aggregate_weights*exp_loss_weights, axis=0).flatten()
             
             #Bayesian_dict['weights_data'] /= np.max(Bayesian_dict['weights_data'])  # if we want to normalize by maximum
 