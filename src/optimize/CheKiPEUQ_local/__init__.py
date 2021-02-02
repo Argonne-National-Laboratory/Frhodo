e19938347fb31c0869476292a3d438770eef10c3
tsikes <50559900+tsikes@users.noreply.github.com>
2021-02-01 23:37:13 -0600

    Refactoring
    Refactoring to clean up fit_fcn
    Also changing imports slightly

--- src/optimize/CheKiPEUQ_local/__init__.py
+++ src/optimize/CheKiPEUQ_local/__init__.py
@@ -2,7 +2,7 @@
 #Except instead of putting things here directly in __init__, we'll impor them so they are accessible by importing the module.
 import sys; import os
 #sys.path.append(os.path.join(os.path.dirname(__file__), '../')) #if the directory one above is added to sys.path, then InverseProblem can be found for importing from.
-from CheKiPEUQ_local.InverseProblem import *
+from .InverseProblem import *
 #from CheKiPEUQ.plotting_functions import *
 #from CheKiPEUQ.mumpce import *
-from InverseProblem import parameter_estimation
+#from InverseProblem import parameter_estimation
